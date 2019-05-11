06c1874bd3a8d587ba94d61f6955861b689a486c
Vitaliy <43651048+VitaliyOsin@users.noreply.github.com>
2019-05-11 23:00:00 +0700

    Add files via upload

---
+++ fn.js
@@ -0,0 +1,28 @@
+var kupAjax = function (method, page, asyn){
+    var xhr; 
+    if(window.XMLHttpRequest){ 
+      xhr = new XMLHttpRequest();
+    }else if(window.ActiveXObject){
+      try{
+        xhr = new ActiveXObject('Microsoft.XMLHTTP');
+      }catch{
+        xhr = new ActiveXObject('Msxm12.XMLHTTP');
+      }
+     }
+    xhr.open(method, page, asyn);
+    xhr.send();
+    if (xhr.status != 200) {
+      // обработать ошибку
+       alert( xhr.status + ': ' + xhr.statusText + ' что-то не так'); // пример вывода: 404: Not Found
+       return;
+    } else {
+    // вывести результат
+    var arrContent = xhr.responseText;
+    //var arrSys = eval('('+arrContent+')'); 
+    return arrContent;     
+    }
+  }
+
+  let copyObj = function(obj){return Object.fromEntries(Object.entries(obj))}
+
+  
